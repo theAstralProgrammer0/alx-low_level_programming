560e9765c867312d2bf74873f5d8acf8974fb2aa
NairaMescudi <tdaisioso@gmail.com>
2023-10-05 14:40:59 +0000

    Added C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Size of a char: 1 byte\(s\)");
+	puts("Size of an int: 4 byte\(s\)");
+	puts("Size of a long int: 4 byte\(s\)");
+	puts("Size of a long long int: 8 byte\(s\)");
+	puts("Size of a float: 4 byte\(s\)");
+	return (0);
+}
