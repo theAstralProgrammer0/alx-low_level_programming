78a313d81339b773f03c537379230b9f00e0e52f
NairaMescudi <tdaisioso@gmail.com>
2023-10-06 01:09:50 +0000

    A C program that prints exactly a string to the standard error

---
+++ 0x00-hello_world/1000-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <errno.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	errno = 0;
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (errno);
+}
--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 1 (Error)
  */
+/**
+ * cutchar - Reusable function
+ *
+ * s - holder
+*/
 
 int main(void)
 {
-	errno = 1;
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (errno);
+	const char *msg =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = 0;
+
+	while (msg[len])
+	{
+		 len++;
+	}
+
+	if (write(STDERR_FILENO, msg, len) == -1)
+	{
+		return (1);
+	}
+
+	return (1);
 }
