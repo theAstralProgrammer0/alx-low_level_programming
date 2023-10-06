26601eacfca3833a1077eaa90430f3a20ffeea56
NairaMescudi <tdaisioso@gmail.com>
2023-10-06 14:47:21 +0000

    Program performs same function as 5- but uses putchar ASCII conversion mechanism

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Return: Always 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	int i;
+
+	while (i <= 9)	
+	{
+		putchar(i +'0');
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
