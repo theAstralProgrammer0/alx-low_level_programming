ea0d266d36d73980a48cb03051e0661cd49977fd
NairaMescudi <tdaisioso@gmail.com>
2023-10-06 16:09:56 +0000

    Hexadecimal version of task 2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  *Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	char str[] = "123456789abcdef";
+
+	int i = 0;
+
+	while (i <= 15)
+	{
+		putchar(str[i]);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
