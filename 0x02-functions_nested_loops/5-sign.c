e1df128f6602a80b9ea2e4080ebc5faa80cdd699
NairaMescudi <tdaisioso@gmail.com>
2023-10-10 17:31:05 +0000

    Added 5-sign.c C program that prints the sign of a character

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * print_sign - Entry Point
+  *
+  * _putchar - function to print a character
+  *
+  * @n: signed integer from main.c arg
+  *
+  * Return: Always 0 (Success)
+  *
+  */
+
+
+int print_sign(int n)
+{
+	switch (n < 0)
+	{
+		case 1:
+			_putchar('-');
+			return (-1);
+		default:
+			switch (n == 0)
+			{
+				case 1:
+					_putchar('0');
+					return (0);
+				default:
+					switch (n > 0)
+					{
+						case 1:
+							_putchar('+');
+							return(1);
+						default:
+							break;
+					}
+			}
+	}
+	return (0);
+}
+/**{
+	switch (n < 0)
+	{
+		case 1:
+			return (-1);
+		default:
+			switch (n == 0)
+			{
+				case 1:
+					return (0);
+				default:
+					switch (n > 0)
+					{
+						case 1:
+							return (1);
+						default:
+							break;
+					}
+			}
+	}
+}
+8
+*/
+
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -11,11 +11,24 @@ int main(void)
 {
     int r;
 
-    r = _islower('H');
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
-    r = _islower('o');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
-    r = _islower(108);
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
