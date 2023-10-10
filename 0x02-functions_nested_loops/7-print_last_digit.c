349ed55893f92d9559467774ab042dc96cd21f04
NairaMescudi <tdaisioso@gmail.com>
2023-10-10 18:27:27 +0000

    Added 7-print_last_digit.c C program that prints the last digit of the absolute value of an integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,18 +17,17 @@
 int print_last_digit(int n)
 {
 	int m;
-	int p;
 
 	switch (n < 0)
 	{
 		case 1:
 			n = n + 2 * (-n);
 			m = n % 10;
-			p = _putchar(m + '0');
-			return (p);
+			_putchar(m + '0');
+			return (m);
 		default:
 			m = n % 10;
-			p = _putchar(m + '0');
-			return (p);
+			_putchar(m + '0');
+			return (m);
 	}
 }
