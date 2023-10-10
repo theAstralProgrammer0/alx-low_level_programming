280af60d077083ef1ad18b672830f916b6b8e8a2
NairaMescudi <tdaisioso@gmail.com>
2023-10-10 14:24:18 +0000

    Added new 0x02-functions_nested_loops directory

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,6 +12,13 @@
 
 int main(void)
 {
-	_putchar("\_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return(0);
 }
