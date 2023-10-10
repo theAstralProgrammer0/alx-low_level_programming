0739458b70cf8462db19bd2653a01b5b7b0b22cd
NairaMescudi <tdaisioso@gmail.com>
2023-10-10 18:10:42 +0000

    Added 7-print_last_digit.c C program that prints the last digit of the abdsolute value of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+
+/**
+  * print_last_digit - Entry Point
+  *
+  * @n: signed integer from main.c arg
+  *
+  * Return: Always 0 (Success)
+  *
+  */
+
+
+int print_last_digit(int n)
+{
+	int m;
+
+	switch (n < 0)
+	{
+		case 1:
+			n = n + 2 * (-n);
+			m = n % 10;
+			return (m);
+		default:
+			m = n % 10;
+			return (m);
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -11,13 +11,10 @@ int main(void)
 {
     int r;
 
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
     return (0);
 }
