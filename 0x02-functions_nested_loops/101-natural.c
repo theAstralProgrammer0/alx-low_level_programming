9bcbf12bd3e8163b814ccee729c37cb87bc6b056
NairaMescudi <tdaisioso@gmail.com>
2023-10-11 18:54:50 +0000

    Added aC program to find sum of multiples of two numbers and prints result to screen

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d", sum);
+	printf("\n");
+
+	return (0);
+}
+
