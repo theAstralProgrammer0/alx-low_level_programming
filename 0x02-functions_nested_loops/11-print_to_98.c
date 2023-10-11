043a5dde6f43f79f5a172fd3968275813b40cbb8
NairaMescudi <tdaisioso@gmail.com>
2023-10-11 16:00:11 +0000

    Used printf for task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,27 @@
+#include <stdio.h>
+
 /**
   * print_to_98 - Entry Point
   *
   * @n: integer argument from function call
   */
 
-
-/**void print_to_98(int n)
-*{
-*	if (n <= 98)
-*	{
-*		print_n_less_equal_98(n);
-*	}
-*	else
-*	{
-*		print_n_greater_98(n);
-*	}
-*	putchar('\n');
-*}
-*/
-#include <stdio.h>
-
-/**
- * print_to_98 - print the number given up to 98
- * @k: this is the number to start from
- * Return: this function does not return anything
- */
-void print_to_98(int k)
+void print_to_98(int n)
 {
-	if (k < 98)
+	if (n < 98)
 	{
-		while (k < 98)
+		while (n < 98)
 		{
-			printf("%d, ", k);
-				k++;
+			printf("%d, ", n);
+				n++;
 		}
 	}
-	else if (k > 98)
+	else if (n > 98)
 	{
-		while (k > 98)
+		while (n > 98)
 		{
-			printf("%d, ", k);
-			k--;
+			printf("%d, ", n);
+			n--;
 		}
 	}
 	printf("98\n");
