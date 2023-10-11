a1f813a3d79450c91b9a991da08c2f6589bd2c7d
NairaMescudi <tdaisioso@gmail.com>
2023-10-11 03:31:13 +0000

    Added 11-print_to_99

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
 void print_to_98(int n)
 {
 	if (n < 98)
@@ -32,4 +33,69 @@ void print_to_98(int n)
 			}
 		}
 	}
+}
+*/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		int i;
+		for (i = n; i <= 98; i++)
+		{
+			int num = i;
+			int div = 1;
+			if (num < 0)
+			{
+				putchar('-');
+				num = -num;
+			}
+			
+			while (num / div  > 9)
+			{
+				div *= 10;
+			}
+			while (div > 0)
+			{
+				putchar(num / div + '0');
+				num %= div;
+				div /= 10;
+			}
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		int i;
+		for (i = n; i >= 98; i--)
+		{
+			int num = i;
+			int div = 1;
+			if (num < 0)
+			{
+				putchar('-');
+				num = -num;
+			}
+			while (num / div > 9)
+			{
+				div *= 10;
+			}
+			while (div > 0)
+			{
+				putchar(num / div + '0');
+				num %= div;
+				div /= 10;
+			}
+			if (i > 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,7 +9,10 @@
 
 int main(void)
 {
-	times_table();
-	return (0);
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+    return (0);
 }
-
