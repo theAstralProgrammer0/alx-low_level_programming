9e2089fdc0ae7ba34a38923e32c74458cf571d2b
NairaMescudi <tdaisioso@gmail.com>
2023-10-11 18:12:02 +0000

    Added C program to print in an orderly fashion, table 0 - 15

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -5,10 +5,11 @@
  * main - check the code
  *
  * Return: Always 0.
- */
+*/
 
 int main(void)
 {
+	print_times_table(2);
     print_times_table(3);
     _putchar('\n');
     print_times_table(5);
@@ -18,3 +19,5 @@ int main(void)
     print_times_table(12);  
     return (0);
 }
+
+
