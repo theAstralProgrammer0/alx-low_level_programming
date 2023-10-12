6b09dab52b457f4a0521df0db883c5e5727e40eb
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 14:07:02 +0000

    A C program to print a triangle of a specific size

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,19 +14,23 @@ void print_triangle(int size)
 	int i;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			if (j <= size - i)
+			for (j = 1; j <= size; j++)
 			{
-				putchar(' ');
-			}
-			else
-			{
-				putchar('#');
-			}
+				if (j <= size - i)
+				{
+					putchar(' ');
+				}
+				else
+				{
+					putchar('#');
+				}
+			}	
+			putchar('\n');
 		}
-		putchar('\n');
 	}
+	putchar('\n');
 }
