33e63d1f747573b8d9be243d9713892a9ce019de
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 22:07:16 +0000

    Fixed leap year bug task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,5 +11,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
