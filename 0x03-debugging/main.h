df4f38c1528fb81561023734f622fadefa1a2da2
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 22:17:32 +0000

    FIxed leap year bug in task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -21,4 +21,7 @@ int a_largest(int a, int b, int c);
 int b_largest(int a, int b, int c);
 int c_largest(int a, int b, int c);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+	
 #endif
