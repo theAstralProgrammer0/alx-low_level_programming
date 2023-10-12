88261b8faba73b7bbb828d94aee236aeaff5b3b2
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 14:32:07 +0000

    FizzBuzz C Program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Description: This is the legendary FizzBuzz code
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int num;
+	char F[] = "Fizz";
+	char B[] = "Buzz";
+	char FB[] = "FizzBuzz";
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+			printf("%s ", F);
+		else if (num % 5 == 0)
+			printf("%s ", B);
+		else if (num % 15 == 0)
+			printf("%s ", FB);
+		else
+			printf("%d ", num);
+	}
+	putchar('\n');
+}
