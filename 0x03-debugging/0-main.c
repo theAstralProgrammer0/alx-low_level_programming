86dcb00cba5690b39360ab259b12246ebc25de40
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 05:22:13 +0000

    Avoided infinite loop via commenting

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	positive_or_negative(98);
+	positive_or_negative(0);
 
 	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -13,10 +13,7 @@
 void positive_or_negative(int i)
 {
 	
-	srand(time(0));
-
-	i = rand() - RAND_MAX / 2;
-
+	
 	if (i == 0)
 	{
 		printf("%d is zero\n", i);
