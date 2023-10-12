76d1c380a6f5ed06cc84ffc82879d7ea409dc076
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 02:40:15 +0000

    Fibonacci 102-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Return: Always 0 (Success)
+  *
+  */
+
+int main(void)
+{
+	int n = 98;
+	int a = 1, b = 2;
+	int i;
+
+	printf("%d, %d", a, b);
+
+	for (i = 3; i <= n; i++)
+	{
+		int c = a + b;
+		printf(", %d", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
