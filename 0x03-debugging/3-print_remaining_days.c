ba905761ec8bea28d747db127de78cc59d7111d7
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 23:05:15 +0000

    Fixed leap year bug finally

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ void print_remaining_days(int month, int day, int year)
 			(year % 400 == 0) ||
 			(year % 400 == 0 && year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
