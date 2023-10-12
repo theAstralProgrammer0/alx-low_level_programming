68a474d67a1b62fa5fc05a642175cea181a03e0f
NairaMescudi <tdaisioso@gmail.com>
2023-10-12 22:56:50 +0000

    Hope diagonal bbug is finally fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,20 +14,19 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	if (n > 0)
+	if (n <= 0)
+		putchar('\n');
+
+	else
 	{
-		for (i = 1; i <= n; ++i)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j <= i; ++j)
+			for (j = 1; j <= i; j++)
 			{
 				putchar(' ');
 			}
-			putchar(92);
+			putchar('\\');
 			putchar('\n');
 		}
-	}
-	else if (n <= 0)
-	{
-		putchar('\n');
 	}
 }
