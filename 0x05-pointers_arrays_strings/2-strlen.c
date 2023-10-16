c6e1aec14c21ba0f7414ef9ebb9104fcd1b51e22
NairaMescudi <tdaisioso@gmail.com>
2023-10-16 15:02:41 +0000

    Added a program to count length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * _strlen - Entry Point
+  *
+  * Description: This function counts the number of characters in a string
+  *
+  * @s: string being passed from int main 
+  *
+  * Return: It returns the count of all the characters before the null byte
+  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (1)
+	{
+		if (s[i] == '\0')
+			break;
+		else
+			i++;
+	}
+	return (i);
+}
