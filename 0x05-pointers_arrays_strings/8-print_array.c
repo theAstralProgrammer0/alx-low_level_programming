3265f602636b14b1ab15317ba18627737103907d
NairaMescudi <tdaisioso@gmail.com>
2023-10-16 20:12:35 +0000

    Added a program that prints half of a string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * print_array - Entry Point
+  *
+  * Description: The Program prints the elements of a given array
+  *
+  * @a: Array parameter passed in from int main
+  *
+  * @n: Number of elements in the array passed from int main
+  *
+  * Return: Nothing
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	putchar('\n');
+}
