379e5e663c680828a21b66410c56521910c9e309
NairaMescudi <tdaisioso@gmail.com>
2023-10-16 18:16:42 +0000

    All codebase added

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,7 @@
   *
   * Description: This function counts the number of characters in a string
   *
-  * @s: string being passed from int main 
+  * @s: string being passed from int main
   *
   * Return: It returns the count of all the characters before the null byte
   */
@@ -13,12 +13,13 @@
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (1)
 	{
-		if (s[i] == '\0')
-			break;
-		else
+		if (s[i] != '\0')
 			i++;
+		else
+			break;
 	}
 	return (i);
 }
