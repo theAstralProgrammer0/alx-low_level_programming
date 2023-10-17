1d33f82b5608b144fa2b96c19392fd09a3c53b06
NairaMescudi <tdaisioso@gmail.com>
2023-10-17 00:08:03 +0000

    Added ascii to integer function for converting any character into its integer equivalent

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,16 +13,14 @@ int _atoi(char *s)
 	int neg = 1;
 	unsigned int integer = 0;
 
-	do
-	{
+	do {
 		if (*s == '-')
 			neg = neg * -1;
 		else if (*s >= '0' && *s <= '9')
 			integer = (integer * 10) + (*s - '0');
 		else if (integer > 0)
 			break;
-	}
-	while (*s++);
+	} while (*s++);
 
 	return (neg * integer);
 }
