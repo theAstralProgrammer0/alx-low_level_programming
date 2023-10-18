b933f1f6b91a57b26891e307d16c603917c499b8
NairaMescudi <tdaisioso@gmail.com>
2023-10-18 20:27:37 +0000

    Adjusted for 3-strcmp.c to betty compliance

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+  * _strcmp - Entry Point
+  *
+  * Destination: This function compares two strings and return the difference
+  * between the different characters
+  *
+  * @s1: First string
+  *
+  * @s2: Second string
+  *
+  * Return: (flag) integer
+  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
@@ -18,7 +31,6 @@ int _strcmp(char *s1, char *s2)
 			flag = s1[i] - s2[i];
 			continue;
 		}
-		
 	}
 
 	return (flag);
