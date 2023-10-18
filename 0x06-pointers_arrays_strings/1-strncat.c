1c4255f64c1d539b2e458d2ae9dde6f4ab519ec5
NairaMescudi <tdaisioso@gmail.com>
2023-10-18 19:41:28 +0000

    Updated 1-strncat.c to betty standard

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+  * _strncat - Entry Point
+  *
+  * Description: This function concatenates n bytes of strings from a source
+  * string to a destination buffer.
+  *
+  * @dest: Destination buffer
+  *
+  * @src: Source string
+  *
+  * @n: Maximum number of bytes of string to be concatenated
+  *
+  * Return: (dest) pointer
+  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int len;
-	
-	len = strlen(dest);	
+
+	len = strlen(dest);
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
@@ -15,6 +30,3 @@ char *_strncat(char *dest, char *src, int n)
 
 	return (dest);
 }
-
-
-
