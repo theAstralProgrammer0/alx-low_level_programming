50ff932422d1d6dc948c5cfcae4a4d11bff2b1e6
NairaMescudi <tdaisioso@gmail.com>
2023-10-18 20:02:27 +0000

    Adjusted for loop condition in 3-strcmp.c - added return (0)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,9 +8,14 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
+		{
 			return (0);
+		}
 		else
+		{
 			return (s1[i] - s2[i]);
+		}
 	}
-}
 
+	return (0);
+}
