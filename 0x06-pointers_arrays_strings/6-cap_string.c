3e8612f56e7241fb125ee3e8dce26ed43ae37185
NairaMescudi <tdaisioso@gmail.com>
2023-10-18 21:39:00 +0000

    Edited 6-cap_string.c to account for tab

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,11 +18,13 @@ char *cap_string(char *str)
 
 	for (i = 0; i < len; i++)
 	{
-		if ((str[i] == 33 || str[i] == 34 || str[i] == 40 || str[i] ==
-					41 || str[i] == 44 || str[i] == 46 ||
-					str[i] == 59 || str[i] == 63 || str[i]
-					== 123 || str[i] == 125 || str[i] == 32
-					|| str[i] == '\t' || str[i] == '\n') &&  (str[i + 1] >= 97) && (str[i + 1] <= 122))
+		if ((str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i]
+					== '.' || str[i] == '!' || str[i] == '?'
+					|| str[i] == '"' || str[i] == '(' ||
+					str[i] == ')' || str[i] == '{' || str[i]
+					== '}' || str[i] == '\t' || str[i] ==
+					'\n') &&  (str[i + 1] >= 97) && (str[i +
+						1] <= 122))
 		{
 			str[i + 1] = str[i + 1] - 32;
 		}
