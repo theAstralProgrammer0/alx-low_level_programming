0089376b03a9df43127f3d71c0625960a0c3416d
NairaMescudi <tdaisioso@gmail.com>
2023-10-23 15:37:09 +0000

    Scaffolding out codebase

--- 0x07-pointer_arrays_strings/0-main.c
+++ 0x07-pointer_arrays_strings/0-main.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * simple_print_buffer - prints buffer in hexa
+  * @buffer: the address of memory to print
+  * @size: the size of the memory to print
+  */
+
 void simple_print_buffer(char *buffer, unsigned int size)
 {
         unsigned int i;
