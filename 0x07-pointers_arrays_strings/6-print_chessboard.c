e12f5034cb91115ad645763f076e6bd7f32e4416
NairaMescudi <tdaisioso@gmail.com>
2023-10-23 19:42:58 +0000

    Made chessboard printing program betty compliant

--- 0x07-pointers_arrays_strings/6-print_chessboard.c
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+  * print_chessboard - Entry Point
+  *
+  * Description: This is a function that prints a chessboard
+  *
+  * @a: Pointer to an array of characters
+  *
+  * Return: Nothing
+  */
 
 void print_chessboard(char (*a)[8])
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			putchar(a[i][j]);
+			_putchar(a[i][j]);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
