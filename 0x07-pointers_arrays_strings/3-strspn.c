77490212a9ae2b1ee8f4aee5d4a3923ed74514a9
NairaMescudi <tdaisioso@gmail.com>
2023-10-23 22:25:42 +0000

    Faith in tweak to solve last correct output check from checker

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/**
+  * _strspn - Entry Point
+  *
+  * Description: This is a function that returns the number of bytes in the
+  * initial segment of a main string which consist only of bytes from another
+  * string.
+  *
+  * @s: Main String
+  *
+  * @accept: The other string
+  *
+  * Return: (count) integer
+  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, count = 0;
