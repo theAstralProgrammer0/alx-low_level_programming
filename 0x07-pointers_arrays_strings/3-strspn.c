6f6efb31886394f1b49db9f8e77a16047b1a3c8d
NairaMescudi <tdaisioso@gmail.com>
2023-10-23 16:21:06 +0000

    Setting stage to debug code

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				count++;
+		}
+	}
+	return (count);
+}
