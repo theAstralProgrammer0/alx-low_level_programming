36348d215035bbacd21292ba2a01ad757767fa0b
NairaMescudi <tdaisioso@gmail.com>
2023-10-23 23:35:12 +0000

    Added a program to find a substring within a main string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				return (haystack + j);
+			}
+		}
+	}
+	return (NULL);
+}
