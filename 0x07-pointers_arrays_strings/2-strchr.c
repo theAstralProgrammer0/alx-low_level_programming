212ba76d91020bfc021ce228e22c3988a1075125
NairaMescudi <tdaisioso@gmail.com>
2023-10-23 16:10:24 +0000

    Added c program to find a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * _strchr - Entry Point
+  *
+  * Description: This is a function that locates a character in a string
+  *
+  * @s: String to be parsed
+  *
+  * @c: Character to be matched
+  *
+  * Return: (s) pointer
+  */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	return (NULL);
+}
