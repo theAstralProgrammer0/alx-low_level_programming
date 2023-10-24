c0adceb1be7298f8b50c762dbc731d76aec3babf
NairaMescudi <tdaisioso@gmail.com>
2023-10-24 00:49:59 +0000

    Tweaked 2-strchr.c to appease checker

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,14 +16,16 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
+	int flag = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		int flag = 0;
-
-		if (s[i] != c)
+		if (s[i] == c)
+		{
 			flag = 1;
-		if (flag == 0)
 			return (s + i);
+		}
 	}
-	return (NULL);
+	if (flag == 0)
+		return (NULL);
 }
