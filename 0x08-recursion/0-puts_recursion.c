2a013be321f4bda06d5bcb804a0ae9f99cf4cf28
NairaMescudi <tdaisioso@gmail.com>
2023-10-25 10:47:09 +0000

    Made 0-puts_recursion.c betty compliant

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/**
+  * _puts_recursion - Entry Point
+  *
+  * Description: This function prints a string by printing each character
+  * recursively
+  *
+  * @s: String to be printed
+  *
+  * Return: Nothing
+  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
