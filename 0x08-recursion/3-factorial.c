358242e51e8b9c4539be9f3fb679e7f6216b9e14
NairaMescudi <tdaisioso@gmail.com>
2023-10-25 20:24:06 +0000

    Added a function that finds the factorial of a number recusively

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+	int s;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+	{
+		s = n * factorial(n - 1);
+		return (s);
+	}
+}
