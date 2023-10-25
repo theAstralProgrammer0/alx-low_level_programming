a3ffc6f87f6d647f9dde9e3691011cf83f0235bc
NairaMescudi <tdaisioso@gmail.com>
2023-10-25 20:49:00 +0000

    Made 4-pow_recursion.c betty compliant

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+  * _pow_recursion - Entry Point
+  *
+  * Description: A function that finds x raised to the power of y
+  *
+  * @x: Mantissa
+  *
+  * @y: Index
+  *
+  * Return: (-1) int, (1) int, (s) int
+  */
+
 int _pow_recursion(int x, int y)
 {
 	int s;
