9f8fba7e9c55a1b843354afb3dc9aeced0723679
NairaMescudi <tdaisioso@gmail.com>
2023-10-26 02:50:10 +0000

    Added a function to print the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+/**
+ * _sqrt_recursion_helper - a helper function that handles the recursion
+ *
+ * @n : input number
+ * @min: minimum number to guess
+ * @max: maximum number to guess
+ *
+ * Return: square root of @n or -1
+*/
+
+int _sqrt_recursion_helper(int n, int min, int max)
+{
+	int guess, guess_squared;
+
+	guess = (min + max) / 2;
+	guess_squared = guess * guess;
+
+	if (guess_squared == n)
+		return (guess);
+	else if (min == max)
+		return (-1);
+	else if (guess_squared < n)
+		return (_sqrt_recursion_wrapper(n, guess + 1, max));
+	else
+		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+}
+
+/**
+ * _sqrt_recursion - a function that returns the natural
+ *                   square root of a number
+ *
+ * @n: input number
+ *
+ * Return: square root
+*/
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	else
+		return (_sqrt_recursion_helper(n, 1, n));
+}
