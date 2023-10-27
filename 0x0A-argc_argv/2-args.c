fe0b0dc1a7eb4129626b6b4951a427fa3a62ae31
NairaMescudi <tdaisioso@gmail.com>
2023-10-27 22:24:29 +0000

    Made 2-args.c betty compliant

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+  * main - Entry Point
+  *
+  * Description: This is a program that prints all the terminal arguments
+  *
+  * @argc: Terminal arguments count
+  *
+  * @argv: Array of terminal arguments
+  *
+  * Return: Always 0 (Success)
+  */
+
 int main(int argc, char *argv[])
 {
 	int i;
