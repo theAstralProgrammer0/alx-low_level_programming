f885a8faa38492200fccd054d80b633addc0d1e7
NairaMescudi <tdaisioso@gmail.com>
2023-10-27 23:09:01 +0000

    Added a function that multiplies two numbers from the terminal

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc >= 2)
+	{
+		int arg1 = atoi(argv[argc - 2]);
+		int arg2 = atoi(argv[argc - 1]);
+		
+		mul = arg1 * arg2;
+		printf("%d\n", mul);
+		return (0);
+	}
+	
+	else
+	{
+		write(1, "Error\n", 7);
+		return (1);
+	}
+}
