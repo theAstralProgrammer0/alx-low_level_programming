fcf9c37728a39420d3df1d864dfa6bcd355855a8
NairaMescudi <tdaisioso@gmail.com>
2023-10-27 23:31:59 +0000

    Made 1-args.c betty compliant

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/**
+  * main - Entry Point
+  *
+  * Description: This is a function that prints the number of arguments in the
+  * terminal
+  *
+  * @argc: Terminal arguments count
+  *
+  * @argv: Array of terminal arguments
+  *
+  * Return: Always 0 (success)
+  */
+
 int main(int argc, char *argv[])
 {
 	if (argv[0])
