ca49f5bb704628498b3dc71fd797da2cb70bf89c
NairaMescudi <tdaisioso@gmail.com>
2023-10-27 21:39:58 +0000

    Made 0-whatsmyname.c betty compliant

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/**
+  * main - Entry Point
+  *
+  * Description: This is a function that prints the name
+  * of the executable compiled into
+  *
+  * @argc: Count of the arguments from the terminal
+  *
+  * @argv: Array of terminal argument strings
+  *
+  * Return: Always 0 (Success)
+  */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
