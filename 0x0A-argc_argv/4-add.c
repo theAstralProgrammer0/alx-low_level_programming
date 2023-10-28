9d08041dc46239044da91d77a764fd1b91cf6ee8
NairaMescudi <tdaisioso@gmail.com>
2023-10-28 00:33:46 +0000

    Added a program that adds numbers from the terminal

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		if (argc == 1)
+			printf("%d\n", 0);
+
+		sum += atoi(argv[i]);
+	}
+	
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/40-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int p;
+	p = atoi(argv[argc - 1]);
+
+	printf("%d", p);
+	return (0);
+}
