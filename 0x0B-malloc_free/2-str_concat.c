da13de0a7a7f223e453b651c4920427afa6e35eb
NairaMescudi <tdaisioso@gmail.com>
2023-10-30 14:58:16 +0000

    Added a program that concatenates two strings together using dynamic mel=mory allocation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+	char *concatstr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 1; *s1 != '\0'; i++)
+	{
+		s1++;
+	}
+	s1 -= i - 1;
+
+	for (j = 1; *s2 != '\0'; j++)
+	{
+		s2++;
+	}
+	s2 -= j - 1;
+
+	concatstr = (char *)calloc(i + j - 1, sizeof(char));
+
+	if (concatstr == NULL)
+		return (NULL);
+
+	for (k = 0; k < i - 1; k++)
+	{
+		*(concatstr + k) = *(s1 + k);
+	}
+	for (k = 0; k < j; k++)
+	{
+		*(concatstr + (i - 1) + k) = *(s2 + k);
+	}
+
+	return (concatstr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,6 @@
 
 char *create_array(unsigned int, char);
 char *_strdup(char *);
+char *str_concat(char *, char *);
 
 #endif
