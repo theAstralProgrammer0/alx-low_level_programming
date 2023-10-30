ef9efdc107c2b3129ca9b4b5ff1751b35bab3666
NairaMescudi <tdaisioso@gmail.com>
2023-10-30 23:27:03 +0000

    Made 4-free_grid.c betty compliant by documenting

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * free_grid - Entry Point
+  *
+  * Description: A function that deallocates memory of a grid that has been
+  * previously allocated
+  *
+  * @grid: Pointer to an array of integer pointers
+  *
+  * @height: Height of the grid
+  *
+  * Return: Nothing
+  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
