9d573f7f1a9b9a93bcca0dc080268c9780957c5f
NairaMescudi <tdaisioso@gmail.com>
2023-10-30 13:57:45 +0000

    Made 1-strdup.c betty compliant

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * _strdup - Entry Point
+  *
+  * Description: A function that allocates memory for a duplicate string
+  * dynamically and copies all the characters in the string to this new
+  * duplicate string location
+  *
+  * @str: String to be parsed
+  *
+  * Return: (dupstr) pointer to duplicated string
+  */
+
 char *_strdup(char *str)
 {
 	if (str == NULL)
 		return (NULL);
-	else
+	int i, j;
+	char *dupstr;
+
+	for (i = 1; *str != '\0'; i++)
 	{
-		int i, j;
-		char *dupstr;
-
-		for (i = 1; *str != '\0'; i++)
-		{
-			str++;
-		}
-
-		str = str - i + 1;
-	
-		dupstr = (char *)calloc(i, sizeof(char));
-
-		if (dupstr == NULL)
-			return (NULL);
-
-		for (j = 0; j < i; j++)
-		{
-			*(dupstr + j) = *(str + j);
-		}
-
-		return (dupstr);
+		str++;
 	}
+	str = str - i + 1;
+	dupstr = (char *)calloc(i, sizeof(char));
+
+	if (dupstr == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+	{
+		*(dupstr + j) = *(str + j);
+	}
+
+	return (dupstr);
 }
