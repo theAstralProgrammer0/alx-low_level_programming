d968196f6236535a8d343c37cf976de654f4d92b
NairaMescudi <tdaisioso@gmail.com>
2023-10-30 21:31:19 +0000

    Reduced height malloc by 1

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = (int *) malloc(height * sizeof(int));
+		grid[i] = (int *) malloc(height - 1 * sizeof(int));
 	}
 
 	if (grid == NULL)
