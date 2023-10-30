5560e8982cf75ab5f4b0188c19f77d79ce8a8c2b
NairaMescudi <tdaisioso@gmail.com>
2023-10-30 11:31:38 +0000

    Made 0-create_array.c betty compliant

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * create_array - Entry Point
+  *
+  * Description: This function creates an array dynamically at runtime and
+  * initializes (populates) the array with a char
+  *
+  * @size: Size of the array to be created
+  *
+  * @c: Character to populate the array with
+  *
+  * Return: (arrptr) pointer to array in heap
+  */
+
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *arrptr = (char *) calloc(size, sizeof(char));
-	
+
 	if (size == 0)
 		return (NULL);
 
