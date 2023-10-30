09a250a80df16b1226b3bdaa9d17a674308c52cd
NairaMescudi <tdaisioso@gmail.com>
2023-10-30 23:14:56 +0000

    Added a free grid function to free all allocated memory from last task

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,5 +5,6 @@ char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
 int **alloc_grid(int, int);
+void free_grid(int **, int);
 
 #endif
