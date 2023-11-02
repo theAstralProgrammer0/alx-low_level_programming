3cf6c9ded83c34b42dcfe791443cdb077694563b
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 23:51:18 +0000

    Made 100-realloc.c betty compliant

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+  * alloc - Auxilliary Function
+  *
+  * Description: A function used to allocate a size of memory using malloc and
+  * check if it returns NULL
+  *
+  * @size: Size to be allocated
+  *
+  * Return: (allocd_ptr) to the newly allocated memory
+  */
+
 void *alloc(unsigned int size)
 {
 	void *allocd_ptr = malloc(size);
@@ -11,6 +22,23 @@ void *alloc(unsigned int size)
 
 	return (allocd_ptr);
 }
+
+/**
+  * _realloc - Entry Point
+  *
+  * Description: A funcion to reallocate memory from an old size to a new size
+  * copying anything stored in the old memory and populating the newly
+  * allocated memory of size new_size with the existing content of the old
+  * memory
+  *
+  * @ptr: Old memory allocated using either malloc or calloc
+  *
+  * @old_size: Size of previously allocated memory
+  *
+  * @new_size: Size of newly allocated memory
+  *
+  * Return: (new_ptr) to newly allocated memory
+  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -39,7 +67,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		min_size = (old_size < new_size) ? old_size : new_size;
 
 		memcpy(new_ptr, ptr, min_size);
-		
+
 		free(ptr);
 	}
 
