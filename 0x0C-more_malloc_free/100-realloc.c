5b1070227af633eea3054012f91d76c20a69cfc6
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 23:38:16 +0000

    Used ternary condition to check for min_size which was used for memcpy

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,31 +16,30 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-	if (ptr == NULL || old_size == 0)
+	if (ptr == NULL)
 	{
 		new_ptr = alloc(new_size);
 	}
 
-	if (ptr != NULL && new_size == 0)
+	else if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (new_size > old_size)
+	else if (new_size == old_size)
+		new_ptr = ptr;
+
+	else
 	{
+		unsigned int min_size;
+
 		new_ptr = alloc(new_size);
+
+		min_size = (old_size < new_size) ? old_size : new_size;
+
 		memcpy(new_ptr, ptr, old_size);
-		free(ptr);
-	}
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (new_size < old_size)
-	{
-		new_ptr = alloc(new_size);
-		memcpy(new_ptr, ptr, new_size);
+		
 		free(ptr);
 	}
 
