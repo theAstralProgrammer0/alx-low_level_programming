7487c3f12cde5fc37a2c7ba093edd5db75f0004e
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 23:39:46 +0000

    Update memcpy

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,7 +38,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		min_size = (old_size < new_size) ? old_size : new_size;
 
-		memcpy(new_ptr, ptr, old_size);
+		memcpy(new_ptr, ptr, min_size);
 		
 		free(ptr);
 	}
