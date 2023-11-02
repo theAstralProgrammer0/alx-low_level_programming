7d92905f9ba7cfed35cb01d19938be3c728a1b7b
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 12:52:57 +0000

    Made 3-array_range.c betty compliant

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * array_range - Entry Point
+  *
+  * Description: A function that lists an array range from min to max
+  *
+  * @min: Minimum array value
+  *
+  * @max: Maximum array value
+  *
+  * Return: (ptr) to new array range
+  */
+
 int *array_range(int min, int max)
 {
 	int i, nmemb, *ptr;
@@ -9,7 +21,7 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	nmemb = max - min + 1;
-	
+
 	ptr = (int *) calloc(nmemb, sizeof(int));
 
 	if (ptr == NULL)
