c5d847533ea73b4322d58752952392b42656ea77
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 12:41:09 +0000

    Made 0-malloc_checked.c file betty compliant

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * malloc_checked - Entry Point
+  *
+  * Description: A function that allocates memory and exits with 98 if memory
+  * cannot be allocated
+  *
+  * @b: Size to be allocated
+  *
+  * Return: (ptr) new memory
+  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
