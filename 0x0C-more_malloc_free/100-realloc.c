53560ecd2170938c1936fd4487f3790c28ff1487
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 18:03:35 +0000

    Added 100-realloc.c function that mimics realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *alloc(unsigned int size)
+{
+	void *allocd_ptr = malloc(size);
+
+	if (allocd_ptr == NULL)
+		return (NULL);
+
+	return (allocd_ptr);
+}
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL)
+	{
+		new_ptr = alloc(new_size);
+	}
+
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size > old_size)
+	{
+		new_ptr = alloc(new_size);
+		memcpy(new_ptr, ptr, old_size);
+		free(ptr);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size < old_size)
+	{
+		new_ptr = alloc(new_size);
+		memcpy(new_ptr, ptr, new_size);
+		free(ptr);
+	}
+
+	return (new_ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,8 @@ char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
 void *_realloc(void *, unsigned int, unsigned int);
+void *alloc(unsigned int);
+void free_here(void *, unsigned int);
+void *copy(void *, unsigned int);
 
 #endif
