b6ea2fd9a7f16ad5367c94bef63539fa7fdb4350
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 16:11:05 +0000

    Added new function to main.h file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,6 @@ void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
 
 #endif
