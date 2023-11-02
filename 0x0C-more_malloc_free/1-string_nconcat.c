94ba95c20ab9d6027b28ecd470e972f798d05fc2
NairaMescudi <tdaisioso@gmail.com>
2023-11-02 08:58:39 +0000

    Added a function that concatenates two strings with a couple conditions

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, lens1, lens2, position;
+	char *s3;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+		lens1 = 1;
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+		lens2 = 1;
+	}
+
+	lens1 = strlen(s1);
+	lens2 = strlen(s2);
+
+	if (n >= lens2)
+	{
+		n = lens2;
+	}
+
+	s3 = (char *) malloc(lens1 + n + 1);
+
+	if (s3 == NULL)
+		return (NULL);
+
+	for (i = 0, position = 0; i < lens1; i++, position++)
+	{
+		s3[position] = s1[i];
+	}
+
+	for (i = 0; i < n; i++, position++)
+	{
+		s3[position] = s2[i];
+	}
+	s3[position++] = '\0';
+
+	return (s3);
+}
