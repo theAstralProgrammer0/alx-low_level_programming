bb4e0523e92991356a1a47e9f5e557bbb91b236c
NairaMescudi <tdaisioso@gmail.com>
2023-11-03 18:27:19 +0000

    Made 101-mul.c betty compliant

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,6 +8,16 @@ void *alloc(size_t);
 char *multiplyStrings(char *, char *);
 char *convertIntArrToStr(int *, int);
 
+/**
+  * isNumber - Auxilliary Function
+  *
+  * Description: This function checks if a string is a positive number
+  *
+  * @digitstr: The string
+  *
+  * Return: (1) if its a number, (0) otherwise
+  */
+
 int isNumber(char digitstr[])
 {
 	int i, len;
@@ -23,6 +33,15 @@ int isNumber(char digitstr[])
 	return (1);
 }
 
+/**
+  * alloc - Auxilliary Function
+  *
+  * Description: This function allocates memory and check if its allocated
+  *
+  * @size: Size to allocate
+  *
+  * Return: (ptr) to the allocated memory
+  */
 
 void *alloc(size_t size)
 {
@@ -35,6 +54,18 @@ void *alloc(size_t size)
 	return (ptr);
 }
 
+/**
+  * main - Entry Point
+  *
+  * Description: This function multiplies two strings
+  *
+  * @argc: Terminal argument count
+  *
+  * @argv: Terminal argument array
+  *
+  * Return: Always 0 (Success)
+  */
+
 int main(int argc, char *argv[])
 {
 	char *s;
@@ -43,22 +74,32 @@ int main(int argc, char *argv[])
 	n1 = argv[1];
 	n2 = argv[2];
 
-
 	if (argc != 3 || isNumber(n1) == 0 || isNumber(n2) == 0)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 /**	printf("%s, %s", n1, n2);*/
 
 	s = multiplyStrings(n1, n2);
-	
+
 	printf("%s\n", s);
 	free(s);
 
 	return (0);
 }
 
+/**
+  * multiplyStrings - Auxilliary Function
+  *
+  * Description: This function contains the algorithm to multiply two strings
+  *
+  * @num1: First number
+  *
+  * @num2: Second number
+  *
+  * Return: (resultString) to the result string
+  */
 
 char *multiplyStrings(char *num1, char *num2)
 {
@@ -89,14 +130,26 @@ char *multiplyStrings(char *num1, char *num2)
 	return (resultString);
 }
 
-char *convertIntArrToStr(int *result, int arrSize)
+/**
+  * convertIntArrToStr - Auxilliary Function
+  *
+  * Description: A function that converts an integer array to a string
+  *
+  * @result: integer array
+  *
+  * @arrLen: Length of the integer array
+  *
+  * Return: (retString) pointer to converted string
+  */
+
+char *convertIntArrToStr(int *result, int arrLen)
 {
 	int i, idx, leadingZeros = 1;
 	char *retString;
 
-	retString = (char *) alloc(arrSize);
+	retString = (char *) alloc(arrLen);
 
-	for (i = 0; i < arrSize; i++)
+	for (i = 0; i < arrLen; i++)
 	{
 		if (result[i] == 0 && leadingZeros)
 			continue; /**Skip leading zeros*/
@@ -107,7 +160,7 @@ char *convertIntArrToStr(int *result, int arrSize)
 		}
 	}
 
-	retString[arrSize] = '\0';
+	retString[arrLen] = '\0';
 	free(result);
 
 	return (retString);
