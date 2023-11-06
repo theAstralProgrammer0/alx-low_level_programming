ef99665a64d0e1324147fd04165ae16f11547e0d
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 16:00:07 +0000

    Added conditions to appease checker

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,13 @@
 
 void print_dog(struct dog *d)
 {
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->age == NULL)
+		d->age = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
