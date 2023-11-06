625a740367abfc603d2c6b748967805cd34b2a02
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 18:27:54 +0000

    Replaced return with exit(1)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		return;
+		exit(1);
 
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 	printf("Age: %f\n", d->age != 0 ? d->age : 0.0);
