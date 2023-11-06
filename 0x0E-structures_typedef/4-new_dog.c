ed635f2d1e87cf74b5aca908cd4ed6b1fe75e640
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 19:45:17 +0000

    Added a memory alloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,17 +20,28 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	size_t len_name, len_owner;
+	char *new_name, *new_owner;
 	dog_t *new_dog;
 
-	new_dog = (dog_t *) malloc(sizeof(dog_t));
+	len_name = strlen(name);
+	len_owner = strlen(owner);
 
+	new_dog = (dog_t *) malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
+	new_name = (char *) malloc(len_name + 1);
+	new_owner = (char *) malloc(len_owner + 1);
 
-
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
+	new_dog->name = strcpy(new_name, name);
+	new_dog->owner = strcpy(new_owner, owner);
 	new_dog->age = age;
 
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_name);
+		free(new_owner);
+		return (NULL);
+	}
 	return (new_dog);
 }
