735172da7ac0ac42ce4b6c697c897985777fae36
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 12:21:45 +0000

    Made 2-main.c betty compliant

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * main - Entry Point
+  *
+  * Description: This function prints the name of this file
+  *
+  * Return: Nothing
+  */
+
 void main(void)
 {
 	char *filename;
