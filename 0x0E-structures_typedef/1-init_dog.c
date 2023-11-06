c0a9a308d66a1900fdcefad018eee9819d4325e2
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 15:04:23 +0000

    Made init_dog.c betty compliant and initialized d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
-
+/**
+  * init_dog - Entry Point
+  *
+  * Description: This function initializes a struct dog variable
+  * object
+  *
+  * @d: dog variable/object of type struct dog
+  *
+  * @name: name of dog
+  *
+  * @age: age of dog
+  *
+  * @owner: name of owner of dog
+  *
+  * Return: Nothing
+  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d dog1;
+	d = {name, age, owner};
 }
