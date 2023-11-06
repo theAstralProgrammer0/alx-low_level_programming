0a54a4227dfa182e9e04af785282dc032cc86a8f
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 14:38:37 +0000

    Made dog.h betty compliant

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog 
+/**
+  * struct dog - Dog object
+  *
+  * Description: Contains all the members of the class/object "dog"
+  *
+  * @name: Name of dog
+  *
+  * @age: Age of dog
+  *
+  * @owner: Name of Owner of dog
+  */
+
+typedef struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
-}dog;
+} dog;
 
 #endif
