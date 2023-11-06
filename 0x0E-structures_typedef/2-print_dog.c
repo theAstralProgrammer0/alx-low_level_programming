06761cb777504f3519b03bcaa4305dce54eb6e4a
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 18:33:50 +0000

    d->age is unbothered

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,6 +19,6 @@ void print_dog(struct dog *d)
 		exit(1);
 
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	printf("Age: %f\n", d->age != 0 ? d->age : 0.0);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
