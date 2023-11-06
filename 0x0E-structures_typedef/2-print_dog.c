12ba07ebaec9c823f49993b00618e0ec74293d67
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 16:27:04 +0000

    Refactored code to accomodate NULL conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,13 +16,17 @@
 void print_dog(struct dog *d)
 {
 	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)";
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
