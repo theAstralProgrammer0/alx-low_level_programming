e10e5c48eddcc150122f45128d602af4d1f1edba
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 18:05:34 +0000

    Allocated memory and copied all char * of dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -19,16 +20,26 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *new_name, *new_owner;
+	unsigned int len_name, len_owner;
 	dog_t *dog_object_ptr;
 
+	len_name = strlen(name);
+	len_owner = strlen(owner);
+
+	new_name = (char *) malloc(len_name * sizeof(char));
+	new_owner = (char *) malloc(len_owner * sizeof(char));
 	dog_object_ptr = (dog_t *) malloc(sizeof(dog_t));
 
 	if (dog_object_ptr == NULL)
 		return (NULL);
 
-	dog_object_ptr->name = name;
+	memcpy(new_name, name, len_name);
+	memcpy(new_owner, owner, len_owner);
+
+	dog_object_ptr->name = new_name;
 	dog_object_ptr->age = age;
-	dog_object_ptr->owner = owner;
+	dog_object_ptr->owner = new_owner;
 
 	return (dog_object_ptr);
 }
