30d31a68d9696a72b7cd48067068974ab091ca8f
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 19:14:11 +0000

    Reverted to strdup

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,30 +20,16 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	size_t len_name, len_owner;
-	char *new_name, *new_owner;
 	dog_t *new_dog;
 
-	len_name = strlen(name);
-	len_owner = strlen(owner);
-
-	new_name = (char *) malloc(sizeof(name));
-	if (new_name == NULL)
-		return (NULL);
-	new_owner = (char *) malloc(sizeof(owner));
-	if (new_owner == NULL)
-		return (NULL);
-
 	new_dog = (dog_t *) malloc(sizeof(dog_t));
+
 	if (new_dog == NULL)
 		return (NULL);
 
-	memcpy(new_name, name, len_name);
-	memcpy(new_owner, owner, len_owner);
 
-
-	new_dog->name = new_name;
-	new_dog->owner = new_owner;
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
 	new_dog->age = age;
 	
 	return (new_dog);
