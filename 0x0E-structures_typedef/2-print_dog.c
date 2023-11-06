d8d7d572b45cb2282d69561d998e6c6b40ed2624
NairaMescudi <tdaisioso@gmail.com>
2023-11-06 18:38:13 +0000

    Replaced ternary operator with if else

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,9 +16,11 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		exit(1);
+		return;
 
-	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
