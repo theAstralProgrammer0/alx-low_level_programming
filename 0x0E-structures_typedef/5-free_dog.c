b0f4cfe837a3f9a0f19c45c9ef2eff32715e44e3
NairaMescudi <tdaisioso@gmail.com>
2023-11-07 00:46:10 +0000

    Made 5-free_dog.c betty compliant

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+  * free_dog - Entry Point
+  *
+  * Description: A function that frees the entire allocated memory for the
+  * new_dog
+  *
+  * @d: object of data type dog_t
+  *
+  * Return: Nothing
+  */
 
 void free_dog(dog_t *d)
 {
