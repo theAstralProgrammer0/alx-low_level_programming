128d3d66345cf4f5a048bfec0ca98e61c28f8a70
NairaMescudi <tdaisioso@gmail.com>
2023-11-08 10:40:47 +0000

    Made 0-print_name.c betty compliant

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,16 @@
+/**
+  * print_name - Entry Point
+  *
+  * Description: A function that calls other (callback) functions to print a
+  * given name string in different formats
+  *
+  * @name: Name parameter
+  *
+  * @f: Function pointer pointing to the callback functions
+  *
+  * Return: Nothing
+  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	(*f)(name);
