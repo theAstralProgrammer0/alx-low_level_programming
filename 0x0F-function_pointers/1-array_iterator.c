b82ebb39b1c7d11f89310b883f580dbc0b20643a
NairaMescudi <tdaisioso@gmail.com>
2023-11-08 19:45:13 +0000

    Considered if array, action or size are faulty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -20,6 +20,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL || size <= 0)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		(*action)(array[i]);
