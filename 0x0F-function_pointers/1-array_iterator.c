4667408dd26d913b907376e80923cabf8f2362b6
NairaMescudi <tdaisioso@gmail.com>
2023-11-08 10:59:10 +0000

    Made 1-array_iterator.c betty compliant

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,21 @@
 #include <stddef.h>
 
+/**
+  * array_iterator - Entry Point
+  *
+  * Description: A file that iterates over an array of numbers while calling 
+  * separate callbacks (int main) to operate on each of the array indices
+  *
+  * @array: Array to be iterated over
+  *
+  * @size: Size of the array
+  *
+  * @action: Function pointer pointing to different functions depending on
+  * the function address passed in the int main function
+  *
+  * Return: Nothing
+  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
