b66ac6521f7443d6dcff145ab38fea26b68fc65a
NairaMescudi <tdaisioso@gmail.com>
2023-11-08 23:12:51 +0000

    Made 3-op_functions.c betty compliant

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,23 +1,78 @@
+/**
+  * op_add - Auxilliary Function
+  *
+  * Description: Adds two numbers and returns the result
+  *
+  * @a: First number
+  *
+  * @b: Second number
+  *
+  * Return: (a + b) int
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+  * op_sub - Auxilliary Function
+  *
+  * Description: Subtracts two numbers and returns the result
+  *
+  * @a: First number
+  *
+  * @b: Second number
+  *
+  * Return: (a - b) int
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+  * op_mul - Auxilliary Function
+  *
+  * Description: Multiplies two numbers and returns the result
+  *
+  * @a: First number
+  *
+  * @b: Second number
+  *
+  * Return: (a * b) int
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+  * op_div - Auxilliary Function
+  *
+  * Description: Divides two numbers and returns the result
+  *
+  * @a: First number
+  *
+  * @b: Second number
+  *
+  * Return: (a / b) int
+  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
+/**
+  * op_mod - Auxilliary Function
+  *
+  * Description: Finds the modulo of two numbers and returns the result
+  *
+  * @a: First number
+  *
+  * @b: Second number
+  *
+  * Return: (a % b) int
+  */
 int op_mod(int a, int b)
 {
 	return (a % b);
