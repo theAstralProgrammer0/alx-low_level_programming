0880eccac1caef23da5fe88ae9aaa39179736a45
NairaMescudi <tdaisioso@gmail.com>
2023-11-08 22:55:38 +0000

    Refactored if condition to utilize strcmp instead of dereference ops[i].op and s

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "3-calc.h"
 
 int (*get_op_func(char *s))(int, int)
@@ -16,7 +17,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
