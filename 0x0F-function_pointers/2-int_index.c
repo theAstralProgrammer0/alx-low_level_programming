7e73dcc30ee1aa4fbc2cde8c32e7831792867877
NairaMescudi <tdaisioso@gmail.com>
2023-11-08 11:38:49 +0000

    Added a function that finds the index of the first occurence of an integer that meets a specific comparison condition

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,14 @@
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 
 void print_name(char *, void (*f)(char *));
 void array_iterator(int *, size_t, void (*)(int));
+int int_index(int *, int, int (*)(int));
 
 #endif
