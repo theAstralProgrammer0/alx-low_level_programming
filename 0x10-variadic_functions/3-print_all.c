88ab52a62a2fd5aa8d1c421e9d01c350fadde744
NairaMescudi <tdaisioso@gmail.com>
2023-11-09 23:05:45 +0000

    Made file betty compliant

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_all(const char * const format, ...)
+/**
+  * print_all - Entry Point
+  *
+  * Description: This function prints any of 4 data types in a specified format
+  * (comma and space separated)
+  *
+  * @format: Format string
+  *
+  * Return: Nothing
+  */
+
+void print_all(const char *const format, ...)
 {
-	va_list specifier;
-	const char * fmt_string;
-	int i_value;
-	char c_value;
-	float f_value;
-	char *s_value, *separator = "";
+	va_list spec;
+	const char *fmt_string;
+	char *s_value, *sep = "";
 
-	va_start(specifier, format);
+	va_start(spec, format);
 	fmt_string = format;
-
-	if(format)
+	if (format)
 	{
 		while (*fmt_string)
 		{
 			switch (*fmt_string)
 			{
 				case 'c':
-					c_value = va_arg(specifier, int);
-					printf("%s%c",separator, c_value);
+					printf("%s%c", sep, va_arg(spec, int));
 					break;
 				case 'i':
-					i_value = va_arg(specifier, int);
-					printf("%s%i", separator, i_value);
+					printf("%s%i", sep, va_arg(spec, int));
 					break;
 				case 'f':
-					f_value = va_arg(specifier, double);
-					printf("%s%f", separator, f_value);
+					printf("%s%f", sep, va_arg(spec,
+								double));
 					break;
 				case 's':
-					s_value = va_arg(specifier, char *);
+					s_value = va_arg(spec, char *);
 					if (s_value == NULL)
 						s_value = "(nil)";
-					printf("%s%s", separator, s_value);
+					printf("%s%s", sep, s_value);
 					break;
 				default:
 					fmt_string++;
 					continue;
 			}
-			separator = ", ";
+			sep = ", ";
 			fmt_string++;
 		}
 	}
 	putchar('\n');
-	va_end(specifier);
+	va_end(spec);
 }
