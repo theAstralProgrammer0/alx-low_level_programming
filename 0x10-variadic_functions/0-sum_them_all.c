d5b997015ecdc12be02d7ccbd7f101e7925edc8e
NairaMescudi <tdaisioso@gmail.com>
2023-11-09 23:15:25 +0000

    Made 0-sum_them_all.c betty compliant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+  * sum_them_all - Entry Point
+  *
+  * Description: This is a function that sums all integer arguments of a
+  * variadic funtion
+  *
+  * @n: Number of numbers to be summed
+  *
+  * Return: (sun) integer
+  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	size_t i, sum = 0;
