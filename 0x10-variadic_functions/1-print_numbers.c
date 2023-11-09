f23abb7503115dce8e559e8d07fe4a2ce88d5dc1
NairaMescudi <tdaisioso@gmail.com>
2023-11-09 23:12:41 +0000

    Made 1-print_numbers.c betty compliant

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+  * print_numbers - Entry Point
+  *
+  * Description: This function prints numbers in a specified format
+  *
+  * @separator: Separator string
+  *
+  * @n: Number of digits to be printed
+  *
+  * Return: Nothing
+  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	size_t i;
