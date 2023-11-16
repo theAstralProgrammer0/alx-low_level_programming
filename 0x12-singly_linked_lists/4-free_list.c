84f27ba3e405da3cbdd3538f71c03c92b6c20291
NairaMescudi <tdaisioso@gmail.com>
2023-11-16 17:12:29 +0100

    Debug 101

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
 #include "lists.h"
 
+
+
 void free_list(list_t *head)
 {
-	list_t *currentNode, *nextNode;
-
-	currentNode = head;
+	list_t *nextNode;
 
 	while (currentNode)
 	{
-		nextNode = currentNode->next;
-		free(currentNode->str);
-		free(currentNode);
-		currentNode = nextNode;
+		nextNode = head->next;
+		free(head->str);
+		free(head);
+		head = nextNode;
 	}
 }
