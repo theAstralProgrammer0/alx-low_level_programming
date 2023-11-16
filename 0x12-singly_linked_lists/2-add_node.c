5098b45c90c4acc76dc45d64f3809a8122e6b02f
NairaMescudi <tdaisioso@gmail.com>
2023-11-16 15:53:34 +0100

    Made 2-add_note.c betty compliant

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+  * add_node - Entry Point
+  *
+  * Description: This function adds a node to the end of linked list
+  *
+  * @head: Pointer to head pointer
+  *
+  * @str: String to be used
+  *
+  * Return: (newnode) pointer to list_t linked list
+  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode;
