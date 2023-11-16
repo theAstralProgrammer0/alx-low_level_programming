1cc21183cc1e7acdc3fd1447eb0867d5cf1a71c3
NairaMescudi <tdaisioso@gmail.com>
2023-11-16 16:44:34 +0100

    Made a couple adjustments in the if *head == NULL

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,16 +5,23 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newnode, *temp;
+	list_t *newnode;
+	list_t *temp = *head;
 
 	newnode = (list_t *) malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (NULL);
 
 	newnode->str = strdup(str);
 	newnode->len = strlen(str);
 	newnode->next = NULL;
 
 	if (*head == NULL)
-		*head = temp = newnode;
+	{
+		*head = newnode;
+		return (newnode);
+	}
 	
 	while(temp->next != NULL)
 		temp = temp->next;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,5 +20,6 @@ typedef struct list_s
 int print_list(const list_t *);
 size_t list_len(const list_t *);
 list_t *add_node(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
 
 #endif
