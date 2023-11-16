837137870e41b78e1bac864904afa980cdb83999
NairaMescudi <tdaisioso@gmail.com>
2023-11-16 12:45:07 +0100

    Made 1-list_len.c betty compliant

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+  * list_len - Entry Point
+  *
+  * Description: This function finds the number of nodes in a linked list
+  *
+  * @h: head that points to the head node of linked list
+  *
+  * Return: (count) int
+  */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
