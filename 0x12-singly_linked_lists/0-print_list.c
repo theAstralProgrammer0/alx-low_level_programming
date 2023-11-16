f1cdaabf8e3bd50a89250f66ea30f85a1e365ec1
NairaMescudi <tdaisioso@gmail.com>
2023-11-16 10:31:32 +0100

    Made files betty compliant

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+  * print_list - Entry Point
+  *
+  * Description: This function prints the contents of the node in a linked
+  * list
+  *
+  * @h: head pointer to head node
+  *
+  * Return: (count) int
+  */
+
 int print_list(const list_t *h)
 {
 	int count = 0;
@@ -9,12 +20,12 @@ int print_list(const list_t *h)
 	while (h != NULL)
 	{
 		list_t *temp = (list_t *)h;
+
 		if (h->str == NULL)
 		{
 			temp->len = 0;
 			temp->str = "(nil)";
 		}
-			
 		printf("[%d] %s\n", temp->len, temp->str);
 		h = h->next;
 		count++;
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,11 +9,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int print_list(const list_t *);
