445b68420239d7508c3f12b1f86bdb273e197d99
NairaMescudi <tdaisioso@gmail.com>
2023-11-16 17:15:26 +0100

    Made 4-free_list.c betty compliant

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,15 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
+/**
+  * free_list - Entry Point
+  *
+  * Description: This function frees all allocated memory for a linked list
+  *
+  * @head: Head node pointer
+  *
+  * Return: Nothing
+  */
 
 void free_list(list_t *head)
 {
