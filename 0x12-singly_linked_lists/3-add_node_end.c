ba1a86de752c628ddb787f5ee4c69a38414f891e
NairaMescudi <tdaisioso@gmail.com>
2023-11-16 16:48:32 +0100

    Made 3-add_node_end betty compliant

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+  * add_node_end - Entry Point
+  *
+  * Description: This function adds a node to the end of a linked list
+  *
+  * @head: Pointer to head pointer
+  *
+  * @str: String vallue for list node member
+  *
+  * Return: (newnode) pointer to last created node
+  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newnode;
@@ -22,8 +34,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = newnode;
 		return (newnode);
 	}
-	
-	while(temp->next != NULL)
+
+	while (temp->next != NULL)
 		temp = temp->next;
 
 	temp->next = newnode;
