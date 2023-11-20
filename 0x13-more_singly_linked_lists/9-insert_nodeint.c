658e195faf007c90b7c870dfe4a02b3514277322
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 00:24:19 +0100

    Debugging Debugging

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,7 +4,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1, count = 0;
+	unsigned int i = 0, count = 0;
 	listint_t *temp, *newnode, *nextnode;
 
 
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	if (count < idx)
+	if ((count - 1) < idx)
 		return (NULL);
 	else
 	{
