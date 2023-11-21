e3fb89c470041c4dd14fffcdfb88fa4f93644bb4
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 18:08:13 +0100

    Added delete function

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,62 +2,32 @@
 #include <stdlib.h>
 #include "lists.h"
 
-size_t listint_len(const listint_t *h)
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-        int count = 0;
-        listint_t *temp, *head = (listint_t *) h;
+	unsigned int length, i = 0, count = 0;
+	listint_t *prev, *new;
+	listint_t *temp = *head;
 
-        temp = head;
-
+	new = malloc(sizeof(listint_t));
+	if (head == NULL || new == NULL)
+		return (-1);
         while (temp)
         {
                 ++count;
                 temp = temp->next;
         }
-        return (count);
-}
-
-
-int pop_listint(listint_t **head)
-{
-        int data;
-        listint_t *temp;
-
-        if (*head == NULL)
-                return (0);
-
-        temp = *head;
-        *head = (*head)->next;
-        data = temp->n;
-        free(temp);
-        return (data);
-}
-
-
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
-{
-	unsigned int length, i = 0;
-	listint_t *prev, *new;
-	listint_t *temp = *head;
-
-	new = malloc(sizeof(listint_t));
-
-	if (head == NULL || new == NULL)
-		return (-1);
-
-	length = listint_len(*head);
-
+	length = count;
 	if (index > length - 1)
 		return (-1);
-
 	if (index == 0)
 	{
-		pop_listint(head);
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
-
 	temp = *head;
-
 	while (i < index)
 	{
 		prev = temp;
