589cd06dba9e3ba552594006ca2ecb9cdd0b3634
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 01:50:06 +0100

    Made 0- betty compliant

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,15 +17,5 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *);
-size_t listint_len(const listint_t *);
-listint_t *add_nodeint(listint_t **, const int);
-listint_t *add_nodeint_end(listint_t **, const int);
-void free_listint(listint_t *);
-void free_listint2(listint_t **);
-int pop_listint(listint_t **);
-int sum_listint(listint_t *);
-listint_t *get_nodeint_at_index(listint_t *, unsigned int);
-listint_t *insert_nodeint_at_index(listint_t **, unsigned int, int);
-int delete_nodeint_at_index(listint_t **, unsigned int);
 
 #endif
