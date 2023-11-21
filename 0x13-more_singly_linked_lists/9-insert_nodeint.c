50a4e60fd075a031059f725c20942ce70e14237d
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 01:31:37 +0100

    Debugger UNleashed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	newnode->n = n;
+	newnode->next = NULL;
 	
 	if ((count - 1) < idx)
 		return (NULL);
