06711c94e76dccb044a362a0e97298c0c5b51765
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 01:08:16 +0100

    Debugger UNleashed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0, count = 0;
-	listint_t *temp, *newnode, *prevnode;
+	listint_t *temp, *newnode;
 
 	temp = *head;
 
@@ -14,27 +14,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		++count;
 		temp = temp->next;
 	}
-
-	temp = *head;
-
+	newnode = (listint_t *) malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
 	if ((count - 1) < idx)
 		return (NULL);
 	else
 	{
-		newnode = (listint_t *) malloc(sizeof(listint_t));
-		if (newnode == NULL)
-			return (NULL);
+		temp = *head;
 		
-		newnode->n = n;
 
 		while(i < idx)
 		{
-			prevnode = temp;
 			temp = temp->next;
 			++i;
 		}
-		newnode->next = temp;
-		prevnode->next = newnode;
+		newnode->n = n;
+		newnode->next = temp->next;
+		temp->next = newnode;
 	}
 	return (newnode);
 }
