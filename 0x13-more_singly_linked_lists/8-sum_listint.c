b8be089256e7afb29c5f6b54228f4c9e6f708308
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 02:27:37 +0100

    Made 8- betty compliant

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,22 +2,30 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+  * sum_listint - Entry Point
+  * Description: A function that sums up all node integer data and returns the
+  * sum
+  * @head: head pointer
+  * Return: (sum) int
+  */
+
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
 	listint_t *temp;
 
-	if (head == NULL)
-		return (0);
-	else
+	if (head != NULL)
 	{
 		temp = head;
 
-		while(temp)
+		while (temp)
 		{
 			sum += temp->n;
 			temp = temp->next;
 		}
 	}
+	else
+		return (0);
 	return (sum);
 }
