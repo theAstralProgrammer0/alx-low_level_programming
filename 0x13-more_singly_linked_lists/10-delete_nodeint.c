eb0164c9a0844870a5a141e1a8d7110ab4903e1e
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 18:41:33 +0100

    Added delete code

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (temp == NULL || temp->next == NULL)
 			return (-1);
 		temp = temp->next;
-		++i;
+		i++;
 	}
 
 	currNode = temp->next;
