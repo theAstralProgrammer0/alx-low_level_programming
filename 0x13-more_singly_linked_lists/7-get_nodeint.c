1b15a3ee4c0eb2ac098853c6c1bd08a41aae1a55
NairaMescudi <tdaisioso@gmail.com>
2023-11-21 02:27:08 +0100

    Made 7- betty compliant

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,34 +2,43 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+  * get_nodeint_at_index - Entry Point
+  * Description: A function that returns pointer to the data of a node at a
+  * specific index
+  * @head: head pointer
+  * @index: index
+  * Return: (temp) pointer to node at index
+  */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0, i = 0;
 	listint_t *temp;
-	
+
 	if (head == NULL)
 		return (NULL);
 
 	temp = head;
 
-	while(temp)
+	while (temp)
 	{
 		++count;
 		temp = temp->next;
 	}
 
 	temp = head;
-	
-	if ((count - 1) < index)
-		return (NULL);
-	else
+
+	if ((count - 1) >= index)
 	{
-		while(i < index)
+		while (i < index)
 		{
 			temp = temp->next;
 			++i;
 		}
 	}
+	else
+		return (NULL);
 	return (temp);
 }
 
