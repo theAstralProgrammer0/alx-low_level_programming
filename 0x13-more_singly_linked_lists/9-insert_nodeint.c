fdc4e3fbbaf1539a2dfc57add027fcdecfe9c32f
NairaMescudi <tdaisioso@gmail.com>
2023-11-22 06:28:28 +0100

    Tweaks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->n = n;
 	newnode->next = NULL;
 	if ((count - 1) < idx)
-		return (NULL);
+		return (newnode);
 	else if (idx == 0)
 	{
 		newnode->next = *head;
