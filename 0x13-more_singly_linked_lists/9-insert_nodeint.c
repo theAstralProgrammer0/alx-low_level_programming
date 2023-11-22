140091497faecb4fc24e84d57765daa82ad0d83f
NairaMescudi <tdaisioso@gmail.com>
2023-11-22 06:10:35 +0100

    Back to basics

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,13 +14,10 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0, count = 0;
-	listint_t *temp, *newnode, *prevnode;
+	listint_t *temp = *head, *newnode, *prevnode;
 
 	if (head == NULL)
 		return (NULL);
-
-	temp = *head;
-
 	while (temp)
 	{
 		++count;
