34462ce75ffd02c51cb932455677afd771d3ddd6
NairaMescudi <tdaisioso@gmail.com>
2023-11-23 20:09:51 +0100

    Made get_bit betty compliant

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * get_bit - Entry Point
+  *
+  * Description: A function that returns the bit of at an index
+  *
+  * @n: Number to be parsed
+  *
+  * @index: Bit index
+  *
+  * Return: 0 (success), 1 (sucess), -1 (failure)
+  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = sizeof(n) * 8 - 1;
