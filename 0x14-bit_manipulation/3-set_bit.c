bb029b30a61e6c51519b26a4555a901b5ece27b1
NairaMescudi <tdaisioso@gmail.com>
2023-11-23 23:52:42 +0100

    Made 3-set_bit betty compliant

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * set_bit - Entry Point
+  *
+  * Description: This is a function that sets a number's bit to 1 at a
+  * specified index
+  *
+  * @n: Pointer to the number
+  *
+  * @index: The index
+  *
+  * Return: 1 (Success), -1 (Failure)
+  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i = sizeof(*n) * 8 - 1;
