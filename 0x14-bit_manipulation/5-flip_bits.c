7da93261c52d0640a6b0a478429b0ab13a268e9d
NairaMescudi <tdaisioso@gmail.com>
2023-11-24 00:04:47 +0100

    Made 5-flip_bits betty compliant

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * flip_bits - Entry Point
+  *
+  * Description: This is a program that counts how many bit flips are needed to
+  * convert a number to another number
+  *
+  * @n: First number
+  *
+  * @m: Second number
+  *
+  * Return: (count) int
+  */
+
 int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int x;
