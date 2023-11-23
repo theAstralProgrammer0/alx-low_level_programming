83f12c024dc385ae53959028b64c8b000881fcb2
NairaMescudi <tdaisioso@gmail.com>
2023-11-23 23:56:06 +0100

    Made 4-clear_bit betty compliant

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * clear_bit - Entry Point
+  *
+  * Description: This is a function that clears a number's bit at a specified
+  * index to 0
+  *
+  * @n: The number
+  *
+  * @index: The index
+  *
+  * Return: 1 (Success), -1 (Failure)
+  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i = sizeof(*n) * 8 - 1;
