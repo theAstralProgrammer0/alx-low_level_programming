7b00ec7496e8d54ec96504cf09667c36d2d44fb6
NairaMescudi <tdaisioso@gmail.com>
2023-11-24 10:15:45 +0100

    Made 100-get_endianness betty compliant

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,18 @@
+/**
+  * get_endianness - Entry Point
+  *
+  * Description: This function finds the endianness of any system
+  *
+  * Return: 1 (Little-endian), 0 (Big-endian)
+  */
+
 int get_endianness(void)
 {
 	int number = 5;
 	int *intptr = &number;
 
 	int val_in_LSB = *((char *)intptr);
-	
+
 	if (val_in_LSB == 5)
 		return (1);
 	else
