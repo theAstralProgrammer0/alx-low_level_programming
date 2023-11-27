d8d14858d175058870374f99f0635d7f1ca22d9d
NairaMescudi <tdaisioso@gmail.com>
2023-11-27 23:01:00 +0100

    Added the read_textfile function

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fileDescriptor;
+	char *buffer;
+	struct stat fileStat;
+	ssize_t bytesWritten = 0, bytesRead = 0, totalBytesRead = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	fileDescriptor = open(filename, O_RDONLY);
+	if (fileDescriptor == -1)
+		return (0);
+
+	if (fstat(fileDescriptor, &fileStat) == -1)
+	{
+		printf("Error getting stat");
+		return (0);
+	}
+
+	buffer = (char *) malloc(fileStat.st_size);
+
+	while (letters)
+	{
+		bytesRead = read(fileDescriptor, buffer, fileStat.st_size);
+		if (bytesRead == -1)
+			return (0);
+		if (bytesRead == 0)
+			break;
+		totalBytesRead += bytesRead;
+		letters -= bytesRead;
+	}
+	bytesWritten = write(STDOUT_FILENO, buffer, fileStat.st_size);
+	if (bytesWritten == -1)
+		return (0);
+	close(fileDescriptor);
+	return (totalBytesRead);
+}
+
+
+
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+<h1>This is the README for the directory on file io</h1>
