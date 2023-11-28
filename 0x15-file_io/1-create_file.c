b18ca8e8526d890a3157581b6dd0ad6325c8f06f
NairaMescudi <tdaisioso@gmail.com>
2023-11-28 01:38:22 +0100

    Empty string text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,36 +9,41 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t bytesWritten = 0;
 	int fileDescriptor;
+	mode_t prevUmask = umask(0);
 
 	if (filename == NULL)
 		return (-1);
 
 	if (access(filename, F_OK) == -1) /**file does not exist*/
 	{
-		fileDescriptor = open(filename,O_CREAT|O_WRONLY|S_IRUSR|S_IWUSR);
+		fileDescriptor =
+			open(filename,O_CREAT|O_WRONLY|0400|0200|O_TRUNC);
 		if (fileDescriptor == -1) return (-1);
-		if (text_content == NULL)
-		{
-			bytesWritten += write(fileDescriptor, "", 1);
-			if (bytesWritten == -1) return (-1);
-		}
-		bytesWritten += write(fileDescriptor, text_content,strlen(text_content));
-		if (bytesWritten == -1) return (-1);
 	}
 	else
 	{
 		fileDescriptor = open(filename,O_WRONLY|O_TRUNC);
 		if (fileDescriptor == -1) return (-1);
-		if (text_content == NULL)
+		if (bytesWritten == -1) return (-1);	
+	}
+
+	if (text_content != NULL)
+	{
+		bytesWritten = write(fileDescriptor, text_content,
+				strlen(text_content));
+		if (bytesWritten == -1)
 		{
-			bytesWritten += write(fileDescriptor, "", 1);
-			if (bytesWritten == -1) return (-1);
-			bytesWritten += write(fileDescriptor, text_content,strlen(text_content));
-			if (bytesWritten == -1) return (-1);
+			close(fileDescriptor);
+			umask(prevUmask);
+			return (-1);
 		}
-		bytesWritten += write(fileDescriptor, text_content,strlen(text_content));
+	}
+	else
+	{
+		bytesWritten = write(fileDescriptor, "", 0);
 		if (bytesWritten == -1) return (-1);
 	}
 	close(fileDescriptor);
+	umask(prevUmask);
 	return (1);
 }
