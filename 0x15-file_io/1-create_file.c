36127d1941661524627dd76d930e6370d1b2ece1
NairaMescudi <tdaisioso@gmail.com>
2023-11-28 01:50:30 +0100

    Made create file betty compliant

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,28 +5,43 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+  * create_file - Entry Point
+  *
+  * Description: This function creates a file if it doesn't exist and writes
+  * text content into it. It truncates the file if it exists and writes text
+  * content into it.
+  *
+  * @filename: File Name
+  *
+  * @text_content: Text to be written
+  *
+  * Return: Always 1 (Success), -1 (Failure)
+  */
+
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t bytesWritten = 0;
 	int fileDescriptor;
-	mode_t prevUmask = umask(0);
 
 	if (filename == NULL)
 		return (-1);
-
 	if (access(filename, F_OK) == -1) /**file does not exist*/
 	{
 		fileDescriptor =
-			open(filename,O_CREAT|O_WRONLY|0400|0200|O_TRUNC);
-		if (fileDescriptor == -1) return (-1);
+			open(filename, O_CREAT | O_WRONLY | 0400 |
+					0200 | O_TRUNC);
+		if (fileDescriptor == -1)
+			return (-1);
 	}
 	else
 	{
-		fileDescriptor = open(filename,O_WRONLY|O_TRUNC);
-		if (fileDescriptor == -1) return (-1);
-		if (bytesWritten == -1) return (-1);	
+		fileDescriptor = open(filename, O_WRONLY | O_TRUNC);
+		if (fileDescriptor == -1)
+			return (-1);
+		if (bytesWritten == -1)
+			return (-1);
 	}
-
 	if (text_content != NULL)
 	{
 		bytesWritten = write(fileDescriptor, text_content,
@@ -34,16 +49,15 @@ int create_file(const char *filename, char *text_content)
 		if (bytesWritten == -1)
 		{
 			close(fileDescriptor);
-			umask(prevUmask);
 			return (-1);
 		}
 	}
 	else
 	{
 		bytesWritten = write(fileDescriptor, "", 0);
-		if (bytesWritten == -1) return (-1);
+		if (bytesWritten == -1)
+			return (-1);
 	}
 	close(fileDescriptor);
-	umask(prevUmask);
 	return (1);
 }
