8aef2c1858c502efa312db85fe58375ce4a9ae11
NairaMescudi <tdaisioso@gmail.com>
2023-11-28 01:16:29 +0100

    Debugging Debugger

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,20 +24,21 @@ int create_file(const char *filename, char *text_content)
 		}
 		bytesWritten += write(fileDescriptor, text_content,strlen(text_content));
 		if (bytesWritten == -1) return (-1);
-		return (1);
 	}
-	else /**file exists*/
+	else
 	{
-		fileDescriptor =
-			open(filename,O_WRONLY|O_TRUNC);
+		fileDescriptor = open(filename,O_WRONLY|O_TRUNC);
 		if (fileDescriptor == -1) return (-1);
 		if (text_content == NULL)
 		{
 			bytesWritten += write(fileDescriptor, "", 1);
 			if (bytesWritten == -1) return (-1);
+			bytesWritten += write(fileDescriptor, text_content,strlen(text_content));
+			if (bytesWritten == -1) return (-1);
+		}
 		bytesWritten += write(fileDescriptor, text_content,strlen(text_content));
 		if (bytesWritten == -1) return (-1);
-		return (1);
 	}
 	close(fileDescriptor);
+	return (1);
 }
