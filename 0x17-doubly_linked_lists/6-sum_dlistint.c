0d183cd2b9c7fa7e6ee523cfe74673f2ad4823c5
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-11 20:55:36 +0100

    Betty compliance

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,16 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - Entry Point
+ *
+ * Description: This function adds integers in each node of a doubly linked
+ * list
+ *
+ * @head: pointer to head node
+ *
+ * Return: (int) sum
+ */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
