428d551c473e1cd255092515430dd29836fcd496
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-11 21:12:54 +0100

    Debugging Debugger

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (temp->next == NULL)
 			return (NULL);
-		if (i != index)
+		if (i < index)
 			temp = temp->next;
 		else
 			return (temp);
