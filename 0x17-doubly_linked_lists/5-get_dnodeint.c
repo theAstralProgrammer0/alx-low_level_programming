75ddb0eca1e57bfd091c607f7f8b5ef3d3fcac1a
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-11 20:53:36 +0100

    Betty compliance

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,18 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - Entry Point
+ *
+ * Description: This function retrives a node at a given index in a doubly
+ * linked list
+ *
+ * @head: pointer to head node
+ *
+ * @index: index where node is to be added
+ *
+ * Return: (dlistint_t *) pointer to node_at_index, (NULL) on failure
+ */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp = NULL;
@@ -13,7 +26,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (temp->next == NULL)
 			return (NULL);
-		
 		if (i != index)
 			temp = temp->next;
 		else
