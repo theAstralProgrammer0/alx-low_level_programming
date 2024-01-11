690480604f5ed39a3c4474ec80849be75e166c1f
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-11 22:54:31 +0100

    Added Debugs

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newnode = NULL;
 	unsigned int i;
 
-	if (*h == NULL || h == NULL)
+	if (h == NULL)
 		return (NULL);
 
 	if (idx == 0)
