de2e01c7ac6309861d3266852a3965e607e55ea7
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-11 20:49:07 +0100

    Betty compliance

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,17 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - Entry Point
+ *
+ * Description: This function frees a doubly linked list
+ *
+ * @head: double pointer to head node
+ *
+ * @n: integer member of node to be added
+ *
+ * Return: (dlistint_t *) pointer to newnode, (NULL) on failure
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current = NULL;
