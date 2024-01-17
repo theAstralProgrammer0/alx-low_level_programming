dabdbdf8dfcd72353919bfb2e49c807f34fccd51
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-17 03:18:59 +0100

    Please work

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/theAstralProgrammer0/alx-low_level_programming/raw/main/0x18-dynamic_libraries/liblucky.so
-export LD_PRELOAD=/tmp/liblucky.so
+wget -P /tmp https://github.com/theAstralProgrammer0/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lucky.so 
+export LD_PRELOAD=/tmp/lucky.so
