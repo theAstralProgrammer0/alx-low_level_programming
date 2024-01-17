9f6af3b1ce5209f84ce168fae42cda05154f5638
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-17 02:22:32 +0100

    Added shared lucky library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/theAstralProgrammer0/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lucky.so 
+wget -P /tmp  
 export LD_PRELOAD=/tmp/lucky.so
