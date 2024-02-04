1e9a808a7b94671ea0d71171e2a57d089b5ccb9a
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-04 02:10:32 +0100

    Delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,14 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - Entry Point
+ *
+ * Description: This function deletes a hash table
+ *
+ * @ht: pointer to hash table
+ *
+ * Return: Nothing
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	free_ht(ht);
