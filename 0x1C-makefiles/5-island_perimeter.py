7f12c468405c25980e5d06c2f198b791f43699ed
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-20 19:35:50 +0100

    Added correct 5-island_perimeter.py file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""This module returns the perimeter of an island described by a grid
-"""
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
+
+
 def island_perimeter(grid):
-	"""
-	This function returns the perimeter of the island described in grid
-	Args:
-		grid is a list of list of integers:
-		0 represents a water zone
-		1 represents a land zone
-		One cell is a square with side length 1
-		Grid cells are not connected diagonally.
-		Grid is rectangular, width and height don’t exceed 100
-	"""
-	perimeter = 0
-	if grid:
-		for row in grid:
-			for cell in row:
-				if cell == 1 and perimeter < 400:
-					perimeter += 4
-	return perimeter
+    """Returns the perimeter of the island in grid."""
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
+
+    return perimeter
