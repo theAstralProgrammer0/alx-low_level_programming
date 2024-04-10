462ddc677d06801f227d6ae3520520e4a0b9c20a
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-04-10 15:22:34 +0100

    fibonacci debug

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,23 +8,19 @@
   */
 
 int main(void)
-{  
+{
 	unsigned long int a = 1, b = 2, next;
-	int n = 98;
-	int i;
+	int n = 98, i;
 
 	printf("%lu, %lu", a, b);
 
 	for (i = 3; i <= n; i++)
 	{
 		next = a + b;
-
 		printf(", %lu", next);
-
 		a = b;
 		b = next;
 	}
-
 	printf("\n");
 
 	return (0);
